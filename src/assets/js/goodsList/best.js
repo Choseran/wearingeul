4f68a7a9bff548b672d2ef050931a5de410caad5
choseran <rlfekrh@gmail.com>
2024-10-14 15:06:03 +0900

    goods tag 종류 추가

--- src/assets/js/goodsList/best.js
+++ src/assets/js/goodsList/best.js
@@ -2,9 +2,9 @@ export const bestGoods = [
     { i: 1, class: ['best'], title: '스마일캣 컬러스와치 명함지 50매', price: '6,000 원'},
     { i: 2, class: ['best'], title: '글입다 자기만의 방 (버지니아 울프) 잉크 30ml', price: '23,000 원'},
     { i: 3, class: ['best'], title: '글입다 행복한 왕자 (오스카 와일드) 잉크 30ml', price: '23,000 원'},
-    { i: 4, class: ['best'], title: '글입다 젊은 왕 (오스카 와일드) 잉크 30ml', price: '23,000 원'},
+    { i: 4, class: ['best', 'soldout'], title: '글입다 젊은 왕 (오스카 와일드) 잉크 30ml', price: '23,000 원'},
     { i: 5, class: ['best'], title: '글입다 나이팅게일과 장미(오스카 와일드) 잉크 30ml', price: '23,000 원'},
     { i: 6, class: ['best'], title: '글입다 한 방울 잉크 컬러스와치 링 타입(100매)', price: '23,000 원'},
-    { i: 7, class: ['best'], title: '[멤버십 전용] 로빈슨 크루소 + Frost 세트 할인', price: '판매가 비공개'},
+    { i: 7, class: ['best', 'md'], title: '[멤버십 전용] 로빈슨 크루소 + Frost 세트 할인', price: '판매가 비공개'},
     { i: 8, class: ['best'], title: '엔키(Enki) 세계 신화 잉크 30ml', price: '23,000 원'},
 ]
