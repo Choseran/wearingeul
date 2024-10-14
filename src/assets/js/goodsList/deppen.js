f2206bbfbc461a47817431f7e525bf70ad7460e7
choseran <rlfekrh@gmail.com>
2024-10-14 15:24:43 +0900

    상품 태그 추가

--- src/assets/js/goodsList/deppen.js
+++ src/assets/js/goodsList/deppen.js
@@ -1,7 +1,7 @@
 export const deppenGoods = [
     { i: 1, title: '글입다 자기만의 방 (버지니아 울프) 잉크 30ml', price: '23,000 원'},
     { i: 2, title: '[모음전] Wearingeul 글입다 문학 잉크 모음', price: '23,000 원'},
-    { i: 3, title: '글입다 젊은 왕 (오스카 와일드) 잉크 30ml', price: '23,000 원'},
+    { i: 3, class: ['best', 'soldout'], title: '글입다 젊은 왕 (오스카 와일드) 잉크 30ml', price: '23,000 원'},
     { i: 4, title: '글입다 잉크 전용 Impression 임프레션 페이퍼 낱장 8절', price: '8,000 원'},
     { i: 5, title: 'Impression 임프레션 드로잉북 B5 (40매)', price: '12,000 원'},
     { i: 6, title: 'Impression 임프레션 드로잉북 A5 (40매)', price: '9,000 원'},
